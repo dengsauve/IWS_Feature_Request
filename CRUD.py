fe68325646d7da8016077b337f328b4178942a96
Dennis <dengsauve@yahoo.com>
2016-09-07 15:20:52 -0700

    CRUD v0.10.3

--- CRUD.py
+++ CRUD.py
@@ -3,20 +3,19 @@ import csv
 
 Mock_Users, Mock_Requests = [], []
 
-with open('documents/User_Data.csv') as f:
-    freader = csv.reader(f)
-    Mock_Users = list(freader)
+# with open('documents/User_Data.csv') as f:
+#     freader = csv.reader(f)
+#     Mock_Users = list(freader)
 
 with open('documents/Request_Data.csv') as g:
     greader = csv.reader(g)
     Mock_Requests = list(greader)
 
-# user_counter = 2
-for user in Mock_Users:
-    new_user = User(username=user[0], email=user[1], password=user[2], isAdmin=False, client_id=user[4])
-    db.session.add(new_user)
+# for user in Mock_Users:
+#     new_user = User(username=user[0], email=user[1], password=user[2], isAdmin=False, client_id=user[4])
+#     db.session.add(new_user)
 
-db.session.flush()
+# db.session.flush()
 
 # for request in Mock_Requests[1:]:
 #     new_request = Request(title=request[0], description=request[1], client_id=request[2], priority=request[3], targetdate=request[4], ticketurl=request[5], productarea_id=request[6], user_id=request[7])
